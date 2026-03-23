1bf3e04f92ca83511b1d87472e3308f8ed52fba7
kuba <jakub.r.pawlowski@gmail.com>
2026-03-23 14:37:39 -0500

    fix: to_snake_case now replaces hyphens with underscores for valid proto filenames

--- lib/string_ext.ml
+++ lib/string_ext.ml
@@ -2,7 +2,12 @@ let to_snake_case s =
   let buf = Buffer.create (String.length s + 4) in
   String.iteri
     (fun i c ->
-      if i > 0 && c >= 'A' && c <= 'Z' then Buffer.add_char buf '_';
-      Buffer.add_char buf (Char.lowercase_ascii c))
+      match (i, c) with
+      | _, '-' -> Buffer.add_char buf '_'
+      | 0, 'A' .. 'Z' -> Buffer.add_char buf (Char.lowercase_ascii c)
+      | _, 'A' .. 'Z' ->
+          Buffer.add_char buf '_';
+          Buffer.add_char buf (Char.lowercase_ascii c)
+      | _ -> Buffer.add_char buf c)
     s;
   Buffer.contents buf
